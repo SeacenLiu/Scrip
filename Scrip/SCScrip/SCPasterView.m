7f351a7391f21a9d6907c1ea0a6d50bf3a7c933b
SeacenLiu <1016667972@qq.com>
2018-04-20 20:48:06 +0800

    贴纸的基本缩放旋转平移。

--- Scrip/SCScrip/SCPasterView.m
+++ Scrip/SCScrip/SCPasterView.m
@@ -11,21 +11,49 @@
 
 #define MAX_FONT_SIZE 500
 
-@interface SCPasterView() <UITextViewDelegate>
+CG_INLINE CGPoint CGRectGetCenter(CGRect rect) {
+    return CGPointMake(CGRectGetMidX(rect), CGRectGetMidY(rect));
+}
+
+CG_INLINE CGFloat CGPointGetDistance(CGPoint point1, CGPoint point2) {
+    //Saving Variables.
+    CGFloat fx = (point2.x - point1.x);
+    CGFloat fy = (point2.y - point1.y);
+    return sqrt((fx*fx + fy*fy));
+}
+
+CG_INLINE CGFloat CGAffineTransformGetAngle(CGAffineTransform t) {
+    return atan2(t.b, t.a);
+}
+
+
+@interface SCPasterView() <UITextViewDelegate, UIGestureRecognizerDelegate>
 {
     CGPoint prevPoint;
     CGPoint touchLocation;
-    
+
     CGPoint beginningPoint;
     CGPoint beginningCenter;
-    
+
     CGRect beginBounds;
-    
+
     CGRect initialBounds;
     CGFloat initialDistance;
-    
+
     CGFloat deltaAngle;
 }
+//@property (nonatomic, assign) CGPoint prevPoint;
+//@property (nonatomic, assign) CGPoint touchLocation;
+//
+//@property (nonatomic, assign) CGPoint beginningPoint;
+//@property (nonatomic, assign) CGPoint beginningCenter;
+//
+//@property (nonatomic, assign) CGRect beginBounds;
+//
+//@property (nonatomic, assign) CGRect initialBounds;
+//@property (nonatomic, assign) CGFloat initialDistance;
+//
+//@property (nonatomic, assign) CGFloat deltaAngle;
 
 /** 删除按钮 */
 @property (nonatomic, strong) UIButton *deleteControl;
@@ -62,6 +90,10 @@ static const CGFloat kIconSize = 24;
         UIFont * font = [UIFont systemFontOfSize:14];
         self.curFont = font;
         self.minFontSize = font.pointSize;
+        self.minSize = CGSizeMake(60, 60 * size.height/size.width);
+        // debug
+        self.layer.borderColor = [UIColor blueColor].CGColor;
+        self.layer.borderWidth = 1;
         // 添加文本框
         [self addSubview:self.textView];
         [self sendSubviewToBack:self.textView];
@@ -88,6 +120,15 @@ static const CGFloat kIconSize = 24;
         
         // 添加拖拽移动手势
         [self addGestureRecognizer:self.moveGesture];
+        self.moveGesture.delegate = self;
+        
+        // 添加放大手势
+        UIPanGestureRecognizer *scalePan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(scaleGestureAction:)];
+        [self.scaleControl addGestureRecognizer:scalePan];
+        
+        // 添加旋转手势
+        UIPanGestureRecognizer *rotatePan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(rotateGestureAction:)];
+        [self.rotateControl addGestureRecognizer:rotatePan];
     }
     return self;
 }
@@ -95,9 +136,76 @@ static const CGFloat kIconSize = 24;
 #pragma mark - action
 - (void)deleteClick {
     NSLog(@"deleteClick");
+    [self removeFromSuperview];
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch {
+    if (touch.view == self.rotateControl || touch.view == self.scaleControl) {
+        return NO;
+    }
+    return YES;
 }
 
 #pragma mark - gesture
+- (void)rotateGestureAction:(UIPanGestureRecognizer*)recognizer {
+    CGPoint center = CGRectGetCenter(self.frame);
+    if ([recognizer state] == UIGestureRecognizerStateBegan) {
+        touchLocation = [recognizer locationInView:self.superview];
+        //求出反正切角
+        deltaAngle = atan2(touchLocation.y-center.y, touchLocation.x-center.x)-CGAffineTransformGetAngle(self.transform);
+        initialBounds = self.bounds;
+        initialDistance = CGPointGetDistance(self.center, touchLocation);
+    } else if ([recognizer state] == UIGestureRecognizerStateChanged) {
+        CGPoint point = [recognizer locationInView:self.superview];
+        float ang = atan2(point.y - self.center.y, point.x - self.center.x);
+        CGFloat angleDiff = deltaAngle - ang;
+        self.transform = CGAffineTransformMakeRotation(-angleDiff);
+    } else if ([recognizer state] == UIGestureRecognizerStateEnded) {
+        
+    }
+}
+
+- (void)scaleGestureAction:(UIPanGestureRecognizer*)recognizer {
+    touchLocation = [recognizer locationInView:self.superview];
+    if ([recognizer state] == UIGestureRecognizerStateBegan) {
+        
+    } else if ([recognizer state] == UIGestureRecognizerStateChanged) {
+        if (self.bounds.size.width < self.minSize.width || self.bounds.size.height < self.minSize.height) {
+            self.bounds = CGRectMake(self.bounds.origin.x, self.bounds.origin.y, self.minSize.width, self.minSize.height);
+            [self layoutSubViewWithFrame:self.bounds];
+            prevPoint = [recognizer locationInView:self];
+        } else {
+            CGPoint point = [recognizer locationInView:self];
+            float wChange = 0.0, hChange = 0.0;
+            wChange = (point.x - prevPoint.x);
+            hChange = (point.y - prevPoint.y);
+            if (ABS(wChange) > 20.0f || ABS(hChange) > 20.0f) {
+                prevPoint = [recognizer locationInView:self];
+                return;
+            }
+            if (wChange < 0.0f && hChange < 0.0f) {
+                float change = MIN(wChange, hChange);
+                wChange = change;
+                hChange = change;
+            }
+            if (wChange < 0.0f) {
+                hChange = wChange;
+            } else if (hChange < 0.0f) {
+                wChange = hChange;
+            } else {
+                float change = MAX(wChange, hChange);
+                wChange = change;
+                hChange = change;
+            }
+            self.bounds = CGRectMake(self.bounds.origin.x, self.bounds.origin.y,
+                                     self.bounds.size.width + (wChange),
+                                     self.bounds.size.height + (hChange));
+            [self layoutSubViewWithFrame:self.bounds];
+            prevPoint = [recognizer locationInView:self];
+        }
+    }
+}
+
 - (void)tapGestureAction:(UITapGestureRecognizer*)recognizer {
     self.select = !self.isSelect;
 }
@@ -130,7 +238,14 @@ static const CGFloat kIconSize = 24;
 
 
 #pragma mark - tool
-
+- (void)layoutSubViewWithFrame:(CGRect)frame {
+    // 计算删除按钮的位置
+    [self.deleteControl setFrame:CGRectMake(0, 0,kIconSize, kIconSize)];
+    // 计算缩放按钮的位置
+    [self.scaleControl setFrame:CGRectMake(frame.size.width-kIconSize, frame.size.height-kIconSize, kIconSize, kIconSize)];
+    // 计算旋转按钮的位置
+    [self.rotateControl setFrame:CGRectMake(frame.size.width-kIconSize, 0, kIconSize, kIconSize)];
+}
 
 #pragma mark - lazy
 - (UIPanGestureRecognizer *)moveGesture {
@@ -184,6 +299,5 @@ static const CGFloat kIconSize = 24;
     }
     return _textView;
 }
-
 
 @end
