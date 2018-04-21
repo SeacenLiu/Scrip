ea4d006350f412f43d4f50160261b25338ee2771
SeacenLiu <1016667972@qq.com>
2018-04-21 20:49:07 +0800

    尝试使用CoreText。

---
+++ Scrip/SCScrip/SCDisplayView.h
@@ -0,0 +1,13 @@
+//
+//  SCDisplayView.h
+//  Scrip
+//
+//  Created by SeacenLiu on 2018/4/21.
+//  Copyright © 2018年 成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCDisplayView : UIView
+
+@end
---
+++ Scrip/SCScrip/SCDisplayView.m
@@ -0,0 +1,18 @@
+//
+//  SCDisplayView.m
+//  Scrip
+//
+//  Created by SeacenLiu on 2018/4/21.
+//  Copyright © 2018年 成. All rights reserved.
+//
+
+#import "SCDisplayView.h"
+#import <CoreText/CoreText.h>
+
+@implementation SCDisplayView
+
+- (void)drawRect:(CGRect)rect {
+    [super drawRect:rect];
+}
+
+@end
